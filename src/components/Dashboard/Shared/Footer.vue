5f896d8d34971773a387cbb3fb3ea1f4d481cb68
bilgi trafiği <bilgitrafigicom@gmail.com>
2021-05-02 03:39:01 +0300

    case

---
+++ src/components/Dashboard/Shared/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="footer">
+    <span class="copyright">
+      Copyright © 2020
+    </span>
+    <a href="https://www.limonist.com/" target="_blank">Limonist</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Footer',
+  components:{}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../../assets/style/shared";
+#footer {
+  position: fixed;
+  bottom: 0;
+  background: $footerBackgroundColor;
+  width: 100%;
+  overflow-x : hidden;
+  padding: 15px;
+  border-top: 1px solid $borderColor;
+
+  .copyright {
+    font-weight: bold;
+    font-size: 14px;
+  }
+
+  a {
+    color: $linkColor;
+  }
+}
+</style>
